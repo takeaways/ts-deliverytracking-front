7d17404cbc4aecbecd3d41d4dbefb454ea04a92d
JangGeonil <>
2020-05-29 10:49:27 +0900

    d

--- src/Modules/Styled/ModalBackground.ts
+++ src/Modules/Styled/ModalBackground.ts
@@ -5,6 +5,10 @@ const ModalBackground = styled(BaseModalBackground)`
 	z-index: 110000;
 	width: 100vw;
 	height: 100vh;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	position: fixed;
 `;
 
 export default ModalBackground;
