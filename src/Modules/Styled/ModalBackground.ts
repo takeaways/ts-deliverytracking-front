3e4601145e4801f2830153a9dfa6ee7b7a8c43a3
JangGeonil <>
2020-05-29 10:44:06 +0900

    background default setting

--- src/Modules/Styled/ModalBackground.ts
+++ src/Modules/Styled/ModalBackground.ts
@@ -3,6 +3,8 @@ import Modal, { BaseModalBackground } from 'styled-react-modal';
 
 const ModalBackground = styled(BaseModalBackground)`
 	z-index: 110000;
+	width: 100vw;
+	height: 100vh;
 `;
 
 export default ModalBackground;
